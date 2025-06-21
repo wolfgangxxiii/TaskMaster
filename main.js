18322e64210167d34f0cf669e99c3f6f3804fad8
Szymon Bławat <120312275+wolfgangxxiii@users.noreply.github.com>
2025-06-21 21:19:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
-document.getElementById('menu-toggle').addEventListener('click', function (e) {
-    var menu = document.getElementById('menu');
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+const menuOverlay = document.getElementById('menu-overlay');
+const closeMenu = document.getElementById('close-menu');
+
+menuToggle.addEventListener('click', function (e) {
     menu.classList.toggle('show-menu');
+    menuOverlay.classList.toggle('show');
     e.stopPropagation();
 });
 
+// Zamknij menu po kliknięciu w overlay
+menuOverlay.addEventListener('click', function () {
+    menu.classList.remove('show-menu');
+    menuOverlay.classList.remove('show');
+});
+
+// Zamknij menu po kliknięciu w krzyżyk
+closeMenu.addEventListener('click', function () {
+    menu.classList.remove('show-menu');
+    menuOverlay.classList.remove('show');
+});
+
+// Zamknij menu po kliknięciu poza menu i hamburgera
 document.addEventListener('click', function (e) {
-    var menu = document.getElementById('menu');
-    var toggle = document.getElementById('menu-toggle');
-    if (!menu.contains(e.target) && !toggle.contains(e.target)) {
+    if (!menu.contains(e.target) && !menuToggle.contains(e.target)) {
         menu.classList.remove('show-menu');
+        menuOverlay.classList.remove('show');
     }
 });
